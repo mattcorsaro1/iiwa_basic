3eb158af18c415320fed60dd43d19e015c4642db
Matt Corsaro <matthew_corsaro@brown.edu>
2019-03-22 15:13:08 -0400

    Option to send home or to a pre-grasp pose directly with no obstacle avoidance.. Don't use.

--- src/basic_iiwa_publisher.cpp
+++ src/basic_iiwa_publisher.cpp
@@ -15,18 +15,49 @@ int main(int argc, char* argv[])
   ros::Publisher joint_pub = n.advertise<iiwa_msgs::JointPosition>("/iiwa/command/JointPosition", 1000);
   ros::Subscriber sub = n.subscribe("/iiwa/state/JointPosition", 1000, joint_print_callback);
 
-  iiwa_msgs::JointPosition target_joints;
-  target_joints.position.a1 = 0.0;
-  target_joints.position.a2 = 0.0;
-  target_joints.position.a3 = 0.0;
-  target_joints.position.a4 = 0.0;
-  target_joints.position.a5 = 0.0;
-  target_joints.position.a6 = 0.0;
-  target_joints.position.a7 = 0.0;
+  iiwa_msgs::JointPosition home_joints;
+  home_joints.position.a1 = 0.0;
+  home_joints.position.a2 = 0.0;
+  home_joints.position.a3 = 0.0;
+  home_joints.position.a4 = 0.0;
+  home_joints.position.a5 = 0.0;
+  home_joints.position.a6 = 0.0;
+  home_joints.position.a7 = 0.0;
+
+  iiwa_msgs::JointPosition grasp_joints;
+  grasp_joints.position.a1 = 0.853;
+  grasp_joints.position.a2 = 1.5604;
+  grasp_joints.position.a3 = 0.0;
+  grasp_joints.position.a4 = 0.0;
+  grasp_joints.position.a5 = 0.0;
+  grasp_joints.position.a6 = 0.0;
+  grasp_joints.position.a7 = 0.0;
+
+  iiwa_msgs::JointPosition pub_joints = grasp_joints;
+  std::string param;
+  if(n.getParam("/basic_iiwa_publisher/pos", param))
+  {
+    std::cout << "Gave param pos: " << param << std::endl;
+    if (param == "grasp")
+    {
+      pub_joints = grasp_joints;
+      std::cout << "Sending to grasp." << std::endl;
+    }
+    else
+    {
+      pub_joints = home_joints;
+      std::cout << "Sending home." << std::endl;
+    }
+  }
+  else
+  {
+    pub_joints = home_joints;
+    std::cout << "Sending home." << std::endl;
+  }
 
   while (ros::ok())
   {
-    joint_pub.publish(target_joints);
+    joint_pub.publish(pub_joints);
     ros::Duration(1.0).sleep();
     ros::spinOnce();
   }
